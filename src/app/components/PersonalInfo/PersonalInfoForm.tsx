eb6a54393fda7ccc3aa040a3aba443ae38e3eb55
ZonirinaNuklear <zo.rakotoson@nuklear.io>
2024-12-14 02:05:20 +0300

    added fields default values

--- src/app/components/PersonalInfo/PersonalInfoForm.tsx
+++ src/app/components/PersonalInfo/PersonalInfoForm.tsx
@@ -10,36 +10,56 @@ const PersonalInfoForm = () => {
         <CustomInput
           id="firstName"
           label="First Name"
+          defaultValue="Peter"
           style={{ flex: "1 1 0%" }}
         />
         <CustomInput
           id="lastName"
           label="Last Name"
+          defaultValue="Cetera"
           style={{ flex: "1 1 0%" }}
         />
       </div>
       <div style={{ display: "flex", gap: "24px" }}>
-        <CustomInput id="city" label="City" style={{ flex: "1 1 0%" }} />
+        <CustomInput
+          id="city"
+          label="City"
+          defaultValue="London"
+          style={{ flex: "1 1 0%" }}
+        />
         <CustomInput
           id="postalCode"
           label="Postal Code"
+          defaultValue="E2 4XF"
           style={{ flex: "1 1 0%" }}
         />
       </div>
-      <CustomInput id="address" label="Address" style={{ flex: "1 1 0%" }} />
+      <CustomInput
+        id="address"
+        label="Address"
+        defaultValue="123 Example"
+        style={{ flex: "1 1 0%" }}
+      />
       <div style={{ display: "flex", gap: "24px" }}>
         <CustomInput
           id="email"
           label="Email"
+          defaultValue="petter@gmail.com"
           type="email"
           style={{ flex: "1 1 0%" }}
         />
-        <CustomInput id="phone" label="Phone" style={{ flex: "1 1 0%" }} />
+        <CustomInput
+          id="phone"
+          label="Phone"
+          defaultValue="+442223334444"
+          style={{ flex: "1 1 0%" }}
+        />
       </div>
       <div style={{ display: "flex", gap: "24px" }}>
         <CustomInput
           id="password"
           label="Password"
+          defaultValue="Password"
           style={{ flex: "1 1 0%" }}
         />
         <div style={{ flex: "1 1 0%" }} />
